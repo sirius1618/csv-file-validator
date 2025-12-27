951d9ad1014031c80a741098e590534ba40a4078
sirius1618 <sagittarius_a1.618@proton.me>
2025-12-27 14:21:03 -0300

    feat: Contruido logica Valida todos os  arquivos que estão dentro do diretorio PENDENTES

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,9 +1,30 @@
 package org.example;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        // aqui vou fazer a chamada
+
+        Path dirPendentes = Paths.get("").toAbsolutePath()
+                .resolve("diretorios-csv")
+                .resolve("PENDENTES");
+
+        try  (var arquivos = Files.list(dirPendentes)) {
+            arquivos.filter(Files::isRegularFile)
+                    .forEach(arquivo -> {
+                        try {
+                            ValidadorCsv validadorCsv =  new ValidadorCsv(arquivo.toString());
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
