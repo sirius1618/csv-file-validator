efd518f4f799ce638d2df406ac3dd210f9c4d24a
sirius1618 <sagittarius_a1.618@proton.me>
2026-01-16 16:57:01 -0300

    refactor: removendo comentarios desnecessarios

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,8 +5,6 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
